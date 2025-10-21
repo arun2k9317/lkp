1b12f312b405670816218e1c5d339c556b8f4197
arun4505 <91312859+arun4505@users.noreply.github.com>
2025-10-22 00:57:26 +0530

    build error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+        <MantineProvider theme={theme}>
           <ModalsProvider>
             <Notifications />
             {children}
